2c9d5e8f5af114dbe5b398f052c6ae5f088c3845
Cleo-Kiama <cleophaskiama7@gmail.com>
2024-01-27 13:46:42 +0300

    removed the test file for the readFile sizes also refactored the multiplexer to use the new style file generator but it still does not handle empty dirs well

--- multiplexer.js
+++ multiplexer.js
@@ -2,7 +2,6 @@ import createStreamSources from "./createFileStreams.js";
 import GenerateFiles from "./generateFiles.js";
 import c from "ansi-colors";
 export function createPacket(path, chunk) {
-  console.log(path);
   const pathBuffer = Buffer.from(path);
   const packet = Buffer.alloc(4 + 1 + pathBuffer.length + chunk.length);
   packet.writeUInt32BE(1 + pathBuffer.length + chunk.length, 0);
@@ -13,65 +12,53 @@ export function createPacket(path, chunk) {
 }
 //TODO whenever it is an empty array of files then it an empty dir so just send with the chunk after the path as Null
 export default async function multiplexer(rootPath, destination) {
-  // TODO change this to use the generate files generator
-  const fileGenerator = new GenerateFiles(rootPath);
-  const iterator = fileGenerator[Symbol.asyncIterator]();
-  let iteratorResult = await iterator.next();
-  const concurrency = 4;
-  let running = 0;
-  const filesPaths = [];
-  const sources = [];
-  //TODO edge case where the initial dir could be empty thus value = []
-  const readMoreFiles = async () => {
-    if (filesPaths.length <= 4 && !iteratorResult.done) {
-      console.log(c.yellow(`reading more files`));
-      
-      filesPaths.push(...iteratorResult.value);
-      let lazyFileStreams = createStreamSources(filesPaths);
-      sources.push(...lazyFileStreams);
+  try {
+    const fileGenerator = new GenerateFiles(rootPath);
+    const iterator = fileGenerator[Symbol.asyncIterator]();
+    let iteratorResult = await iterator.next();
+    while (!iteratorResult.done) {
+      if (Object.hasOwn(iteratorResult.value, "empty")) {
+        console.log(
+          c.red(`encountered an empty dir ${iteratorResult.value.path}`)
+        );
+        continue;
+      }
+      await sendPacket(iteratorResult.value, destination);
       iteratorResult = await iterator.next();
-      console.log(filesPaths);
-    } else {
-      console.log(
-        c.green(
-          `not adding more files as filesPaths.length is :${filesPaths.length}`
-        )
-      );
     }
-  };
-  const readSources = async (concurrency) => {
-    await readMoreFiles()
-    while (running < concurrency && filesPaths.length > 0) {
+  } catch (error) {
+    console.error(`error happened multiplexing  : ${error.message}`);
+  }
+  if(destination.writable) {
+    destination.end()
+  }
+}
+
+async function sendPacket(files, destination) {
+  let sources = createStreamSources(files);
+  let pendingWritingOperations = 0;
+  return new Promise((resolve, reject) => {
+    while (files.length > 0) {
       const currentSource = sources.shift();
-      const currentPath = filesPaths.shift();
-      //TODO take care of the situation where we have an empty files thus chunk might be null
+      const currentPath = files.shift();
+      if (!currentPath) return;
       currentSource.on("readable", function () {
         let chunk;
         while ((chunk = this.read()) !== null) {
           const packet = createPacket(currentPath, chunk);
-          destination.write(packet);
-        }
-      });
-      currentSource.on("error", (error) => {
-        console.error(
-          `error reading a stream source:${currentPath} error : ${error.message}`
-        );
-      });
-      currentSource.on("end", async () => {
-        running -= 1;
-
-        if (!iteratorResult.done) {
-         await readSources(concurrency);
-        } else if (running === 0 && destination.writable) {
-          destination.end(() => {
-            console.log(`stream multiplexing ended with running ${running}`);
+          destination.write(packet, (err) => {
+            if (err) throw new Error(err.message);
+            pendingWritingOperations -= 1;
           });
+          ++pendingWritingOperations;
         }
       });
-
-      running++;
+      currentSource.on("error", reject);
+      currentSource.on("end", () => {
+        if (files.length === 0&&pendingWritingOperations===0) {
+          resolve();
+        }
+      });
     }
-  };
-
-  await readSources(concurrency);
+  });
 }
