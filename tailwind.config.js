a9f3cfc3c229994e0e0ec667ba36c534f2a0a468
CleoKiama <cleophaskiama7@gmail.com>
2024-02-26 07:11:52 +0300

    initial commit for electron integration

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/renderer/**/*.{js,ts,jsx,tsx,mdx}",
+    ".src/renderer/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
