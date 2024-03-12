637c78b51f601e20e1918b4f676c73101985a153
CleoKiama <cleophaskiama7@gmail.com>
2024-03-12 10:08:54 +0300

    updated the multiplexer and demultiplexer to handle the drain event

--- src/backend/demultiplexer.js
+++ src/backend/demultiplexer.js
@@ -1,55 +1,50 @@
-import c from "ansi-colors";
-import DestinationResolver from "./destinationResolver.js";
-
-
-
-
+import c from 'ansi-colors'
+import DestinationResolver from './destinationResolver.js'
 
 export default async function Demultiplexer(source) {
-  let DestinationResolverInstance = new DestinationResolver();
-  let writingOperations = 0;
-  return new Promise((resolve, reject) => {
-    let currentLength = null;
-    let currentPath = null;
-    source.on("readable", () => {
-      let chunk;
-      if (currentLength === null) {
-        chunk = source.read(4);
-        currentLength = chunk && chunk.readUInt32BE(0);
-      }
-      if (currentLength === null) {
-        console.log(`waiting for more data`);
-        return null;
-      }
-      chunk = source.read(currentLength);
-      if (chunk === null) {
-        console.log(`waiting for more data`);
-        return null;
-      }
-      let pathLength = chunk.readUInt8(0);
-      currentPath = chunk.toString("utf8", 1, 1 + pathLength);
-      let contentBuffer = Buffer.alloc(chunk.length - pathLength - 1);
-      if (contentBuffer.length === 0) {
-        contentBuffer = null;
-      } else chunk.copy(contentBuffer, 0, 1 + pathLength, chunk.length);
-      writingOperations++;
-      DestinationResolverInstance
-        .saveToFileSystem(currentPath, contentBuffer)
-        .then(() => {
-          if (--writingOperations === 0 && source.readableEnded) resolve();
-        }).catch(error=>{
-          console.error(c.red(`error writing ${error.message}`))
+    let DestinationResolverInstance = new DestinationResolver()
+    let fsActivity = []
+    let currentLength = null
+    let currentPath = null
+    source.on('readable', () => {
+        let chunk
+        if (currentLength === null) {
+            chunk = source.read(4)
+            currentLength = chunk && chunk.readUInt32BE(0)
+        }
+        if (currentLength === null) {
+            console.log(`waiting for more data`)
+            return null
+        }
+        chunk = source.read(currentLength)
+        if (chunk === null) {
+            console.log(`waiting for more data`)
+            return null
+        }
+        let pathLength = chunk.readUInt8(0)
+        currentPath = chunk.toString('utf8', 1, 1 + pathLength)
+        let contentBuffer = Buffer.alloc(chunk.length - pathLength - 1)
+        if (contentBuffer.length === 0) {
+            contentBuffer = null
+        } else chunk.copy(contentBuffer, 0, 1 + pathLength, chunk.length)
+        console.log(c.green(`arrived ${currentPath} procceding to write it now`))
+        let fsOperation = DestinationResolverInstance.saveToFileSystem(currentPath,contentBuffer)
+        fsActivity.push(fsOperation)
+        currentLength = null
+        currentPath = null
+    })
+
+    return new Promise((resolve, reject) => {
+        source.once('end', () => {
+            console.log(c.magentaBright('The socket end event fired waiting for pending write operations'))
+             Promise.all(fsActivity).then(resolve)
         })
-      currentLength = null;
-      currentPath = null;
-    });
-    source.on("error", (err) => {
-      console.log(c.red(`erro from demux from source.on(err) ${err.message} `));
-      reject(err.message);
-    });
-    source.on("end", () => {
-      if (writingOperations === 0) return resolve();
-    });
-  });
+        source.once('error', (err) => {
+            console.log(
+                c.red(`error from demux from source.on(err) ${err.message} `)
+            )
+            //TODO might need to do better cleanup here instead of just resolving
+            reject(err.message)
+        })
+    })
 }
-//TODO start here future cleo start by doing the manual write stream without involving the dynamic path usage and see if we still get the full data
--- src/backend/multiplexer.js
+++ src/backend/multiplexer.js
@@ -1,105 +1,138 @@
-import createStreamSources from "./createFileStreams.js";
-import GenerateFiles from "./generateFiles.js";
-import c from "ansi-colors";
-import GetRootSourcePath from "./rootSourcePath.js";
-const rootSourcePath = GetRootSourcePath()
+import createStreamSources from './createFileStreams.js'
+import GenerateFiles from './generateFiles.js'
+import path from 'path'
+import c from 'ansi-colors'
 
 export function createPacket(path, chunk) {
-    let finalPath
-    let startIndex = path.indexOf(rootSourcePath)
-    finalPath = path.substring(startIndex)
-  if (chunk === null) {
-    const pathBuffer = Buffer.from(finalPath);
-    const packet = Buffer.alloc(4 + 1 + pathBuffer.length);
-    packet.writeUInt32BE(1 + pathBuffer.length, 0);
-    packet.writeUInt8(pathBuffer.length, 4);
-    pathBuffer.copy(packet, 5, 0, pathBuffer.length);
-    return packet;
-  }
-  const pathBuffer = Buffer.from(finalPath);
-  const packet = Buffer.alloc(4 + 1 + pathBuffer.length + chunk.length);
-  packet.writeUInt32BE(1 + pathBuffer.length + chunk.length, 0);
-  packet.writeUInt8(pathBuffer.length, 4);
-  pathBuffer.copy(packet, 5, 0, pathBuffer.length);
-  chunk.copy(packet, 5 + pathBuffer.length, 0, chunk.length);
-  return packet;
+    if (chunk === null) {
+        const pathBuffer = Buffer.from(path)
+        const packet = Buffer.alloc(4 + 1 + pathBuffer.length)
+        packet.writeUInt32BE(1 + pathBuffer.length, 0)
+        packet.writeUInt8(pathBuffer.length, 4)
+        pathBuffer.copy(packet, 5, 0, pathBuffer.length)
+        return packet
+    }
+    const pathBuffer = Buffer.from(path)
+    const packet = Buffer.alloc(4 + 1 + pathBuffer.length + chunk.length)
+    packet.writeUInt32BE(1 + pathBuffer.length + chunk.length, 0)
+    packet.writeUInt8(pathBuffer.length, 4)
+    pathBuffer.copy(packet, 5, 0, pathBuffer.length)
+    chunk.copy(packet, 5 + pathBuffer.length, 0, chunk.length)
+    return packet
 }
 
 export default async function multiplexer(rootPath, destination) {
-  try {
-    const fileGenerator = new GenerateFiles(rootPath);
-    const iterator = fileGenerator[Symbol.asyncIterator]();
-    let iteratorResult = await iterator.next();
-    while (!iteratorResult.done) {
-      if (Object.hasOwn(iteratorResult.value, "empty")) {
-        await sendEmptyDirPacket(iteratorResult.value.path, destination);
-        iteratorResult = await iterator.next();
-        continue;
-      }
-      await sendPacket(iteratorResult.value, destination);
-      iteratorResult = await iterator.next();
-    }
-  } catch (error) {
-    console.error(`error happened multiplexing  : ${error.message}`);
-  }
-  if (destination.writable) return destination.end();
-}
-const sendEmptyDirPacket = async (emptyDir, destination) => {
-  return new Promise((resolve, reject) => {
-    destination.write(createPacket(emptyDir, null), (err) => {
-      if (err) return reject(err);
-      resolve();
-    });
-  });
-};
-
-async function sendPacket(files, destination) {
-  let sources = createStreamSources(files);
-  let pendingWritingOperations = 0;
-  return new Promise((resolve, reject) => {
-    while (files.length > 0) {
-      const currentSource = sources.shift();
-      const currentPath = files.shift();
-
-      if (!currentPath) return;
-      currentSource.on("readable",async function () {
-        let chunk;
-        while ((chunk = this.read()) !== null) {
-          pendingWritingOperations++;
-          let drain =  destination.write(createPacket(currentPath, chunk), (err) => {
-            if (err) return reject(err);
-            pendingWritingOperations -= 1;
-          });
-          if(!drain) {
-            return new Promise (resolve=>{
-               destination.once("drain",resolve)
-            })
-          }
+    try {
+        const fileGenerator = new GenerateFiles(rootPath)
+        const iterator = fileGenerator[Symbol.asyncIterator]()
+        let iteratorResult = await iterator.next()
+        while (!iteratorResult.done) {
+            if (Object.hasOwn(iteratorResult.value, 'empty')) {
+                await sendEmptyDirPacket(
+                    rootPath,
+                    iteratorResult.value.path,
+                    destination
+                )
+                iteratorResult = await iterator.next()
+                continue
+            }
+            await sendPacket(rootPath, iteratorResult.value, destination)
+            iteratorResult = await iterator.next()
         }
-      });
-      currentSource.on("error", (err) => {
+    } catch (error) {
         console.error(
-          c.red(
-            `error happened while reading file ${currentPath} : ${err.message}`
-          )
-        );
-        reject(err);
-      });
-      currentSource.on("end", () => {
-        const endOfFileMessage = "all done";
-        destination.write(
-          createPacket(currentPath, Buffer.from(endOfFileMessage)),
-          (err) => {
-            if (err) return reject(err);
-            if (files.length === 0 && pendingWritingOperations === 0) {
-            resolve();
-          }
-          }
-        );
-      });
+            c.redBright(`error happened multiplexing  : ${error.message}`)
+        )
+        return Promise.reject(error)
     }
-  });
+    console.log(c.yellow('multiplexer all done ending the socket now'))
+    if (destination.writable) return destination.end()
+}
+const sendEmptyDirPacket = async (rootPath, emptyDirPath, destination) => {
+    let relativePath
+    const basename = `/${path.basename(rootPath)}`
+    // **handling the case where the first emptyDir is also empty
+    // ** as in no files but may have other nested Empty Dirs
+    if (emptyDirPath === rootPath) relativePath = basename
+    else relativePath = `${basename}${emptyDirPath.substring(rootPath.length)}`
+    return new Promise((resolve, reject) => {
+        let drain = destination.write(
+            createPacket(relativePath, null),
+            (err) => {
+                if (err) return reject(err)
+            }
+        )
+        if (drain) return resolve()
+        destination.once('drain', resolve)
+    })
 }
 
-
-
+async function sendPacket(rootPath, files, destination) {
+    const receivedLengthOfFiles = files.length
+    let sources = createStreamSources(files)
+    let pendingWritingOperations = []
+    return new Promise((resolve, reject) => {
+        var awaitPendingWrites = () => {
+            console.log(
+                c.greenBright(
+                    `waiting for the final all done messages to be sent`
+                )
+            )
+            Promise.all(pendingWritingOperations).then(resolve)
+        }
+        while (files.length > 0) {
+            const currentSource = sources.shift()
+            const currentPath = files.shift()
+            if (!currentPath) return
+            const basename = path.basename(rootPath)
+            const relativePath = `/${basename}${currentPath.substring(rootPath.length)}`
+            currentSource.on('readable', async function () {
+                let chunk
+                while ((chunk = this.read()) !== null) {
+                    //TODO handle drain here please
+                    let drain = destination.write(
+                        createPacket(relativePath, chunk),
+                        (err) => {
+                            if (err) return reject(err)
+                        }
+                    )
+                    if (!drain) console.log(c.magentaBright(`waiting for drain event`))
+                    return await new Promise((done) => {
+                        destination.once('drain', done)
+                    })
+                }
+            })
+            currentSource.once('error', (err) => {
+                console.error(
+                    c.red(
+                        `error happened while reading file ${currentPath} : ${err.message}`
+                    )
+                )
+                reject(err)
+            })
+            currentSource.once('end', () => {
+                const endOfFileMessage = 'all done'
+                console.log(
+                    c.bgCyanBright(
+                        `sending end of file message for ${relativePath}`
+                    )
+                )
+                let drain = new Promise((res, rej) => {
+                    destination.write(
+                        createPacket(
+                            relativePath,
+                            Buffer.from(endOfFileMessage)
+                        ),
+                        (err) => {
+                            if (err) return rej(err)
+                            res()
+                        }
+                    )
+                })
+                pendingWritingOperations.push(drain)
+                if (pendingWritingOperations.length === receivedLengthOfFiles)
+                    return awaitPendingWrites()
+            })
+        }
+    })
+}
