23a7aba97ea7142dda5363299860969a9170f893
= <cleophaskiama7@gmail.com>
2024-04-14 09:45:53 +0300

    fixed a typo preventing showing of username of the current device

--- src/renderer/components/thisDevice.jsx
+++ src/renderer/components/thisDevice.jsx
@@ -2,36 +2,35 @@ import React, { useState, useEffect } from 'react'
 import Loader from './loader.jsx'
 
 
-
 export default function ThisDevice() {
-    const [thisDevice, setThisDevice] = useState()
-    const [thisDeviceInfo, setThisDeviceInfo] = useState(false)
-    useEffect(() => {
-        if (thisDeviceInfo) return
-        window.electron.this_device().then((device) => {
-            setThisDevice(device)
-            setThisDeviceInfo(true)
-        })
+  const [thisDevice, setThisDevice] = useState()
+  const [thisDeviceInfo, setThisDeviceInfo] = useState(false)
+  useEffect(() => {
+    if (thisDeviceInfo) return
+    window.electron.this_device().then((device) => {
+      setThisDevice(device)
+      setThisDeviceInfo(true)
     })
-    
-    return (
-        <>
-            {!thisDevice? 
-                <Loader />
-             : 
+  })
+
+  return (
+    <>
+      {!thisDevice ?
+        <Loader />
+        :
         <div className="flex flex-row items-center " >
-             <img
-                src={`static://assets/${thisDevice.type}_logo.svg`}
-                className="h-10 w-10"
-                alt={`${thisDevice.type} platform logo`}
-            />
-            <div className="ml-6 flex flex-col">
-                    <p className="self-start text-sm text-gray-600">you</p>
-                <p>{thisDevice.userInfo.userName}</p>
-            </div>
-             
+          <img
+            src={`static://assets/${thisDevice.type}_logo.svg`}
+            className="h-10 w-10"
+            alt={`${thisDevice.type} platform logo`}
+          />
+          <div className="ml-6 flex flex-col">
+            <p className="self-start text-sm text-gray-600">you</p>
+            <p>{thisDevice.userInfo.username}</p>
+          </div>
+
         </div>
-            }
-        </>
-    )
+      }
+    </>
+  )
 }
