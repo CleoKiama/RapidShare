cca49fcc2ec532c8c2015526e7f5a5d83d9a626f
= <cleophaskiama7@gmail.com>
2024-04-15 08:54:53 +0300

    removed unnecessary prop isCurrentDevice

--- src/renderer/components/device.jsx
+++ src/renderer/components/device.jsx
@@ -2,49 +2,36 @@ import React from 'react'
 import clsx from 'clsx'
 import PropTypes from 'prop-types'
 function Device({ isCurrentDevice, userName, address, platform }) {
-       const blurWindow = ()=> {
-           console.log("Blurring the window until dialogue is closed")
-       }
-       const unBlurWindow =()=>{
-          console.log('unBlurring the window now after dialogue closed')
-       }
-    const handleClick = async () => {
-       console.log(`sending a request to open file dialog to send to addr ${address}`) 
-        blurWindow()
-         const fileStaff = await window.electron.openFileDialog(address)
-          console.log(fileStaff) 
-        unBlurWindow()
-    }
-    return (
-        <div
-            className={clsx(
-                'flex flex-row items-center',
-                !isCurrentDevice && 'py-6'
-            )}
-        >
-            <img
-                src={`static://assets/${platform}_logo.svg`}
-                className="h-10 w-10"
-                alt={`${platform} platform logo`}
-            />
-            <div className="ml-6 flex flex-col">
-                {isCurrentDevice && (
-                    <p className="self-start text-sm text-gray-600">you</p>
-                )}
-                <p>{userName}</p>
-            </div>
-            <button onClick={handleClick} className="rounded-lg px-2 py-6">
-                send a file or folder
-            </button>
-        </div>
-    )
+  console.log(address)
+  const blurWindow = () => {
+    console.log("Blurring the window until dialogue is closed")
+  }
+  const unBlurWindow = () => {
+    console.log('unBlurring the window now after dialogue closed')
+  }
+  const handleClick = async () => {
+    console.log(`sending a request to open file dialog to send to addr ${address}`)
+    blurWindow()
+    const fileStaff = await window.electron.openFileDialog(address)
+    console.log(fileStaff)
+    unBlurWindow()
+  }
+  return (
+    <div className={clsx('flex flex-row items-center', !isCurrentDevice && 'py-6')} >
+      <img
+        src={`static://assets/${platform}_logo.svg`}
+        className="h-10 w-10"
+        alt={`${platform} platform logo`}
+      />
+      <p className='ml-4'>{userName}</p>
+    </div>
+  )
 }
 
 Device.propTypes = {
-    isCurrentDevice: PropTypes.bool.isRequired,
-    userName: PropTypes.string.isRequired,
-    platform: PropTypes.string.isRequired,
-    address: PropTypes.string.isRequired,
+  userName: PropTypes.string.isRequired,
+  platform: PropTypes.string.isRequired,
+  address: PropTypes.string.isRequired,
 }
 
 export default Device
--- src/renderer/components/discoveredDevices.jsx
+++ src/renderer/components/discoveredDevices.jsx
@@ -1,41 +1,53 @@
 import React, { useEffect, useState } from 'react'
 import Device from './device.jsx'
 
-function DiscoveredDevices() {
-    const [deviceData, setDeviceData] = useState()
-    useEffect(() => {
-        function listen(event, data) {
-            console.log('a device found rendering it now')
-            setDeviceData(data)
-        }
-        window.electron.on('deviceFound', listen)
-        window.electron.on('foundDevicesUpdate',listen)
-        return () => {
-            window.electron.removeListener('deviceFound', listen)
-            window.electron.removeListener('foundDevicesUpdate', listen)
-        }
-    }, [])
-    if (!deviceData) {
-        // ** can render something else until a device is discovered
-        return <></>
+let initialData = {
+  devices: [
+    {
+      uid: 1001,
+      gid: 1001,
+      username: 'john',
+      homedir: '/Users/john',
+      shell: '/bin/zsh',
+      address: '192.168.0.104',
+      port: 4000,
+      platform: 'Darwin'
     }
-    const devices = deviceData.devices.map((device) => {
-        console.log("rendering a bunch of buttons")
-        return (
-            <Device
-                key={device.address}
-                isCurrentDevice={false}
-                userName={device.username}
-                platform={device.platform}
-                address = {device.address}
-            />
-        )
-    })
+  ]
+}
+
+function DiscoveredDevices() {
+  const [deviceData, setDeviceData] = useState(initialData)
+  useEffect(() => {
+    function listen(_, data) {
+      setDeviceData(data)
+    }
+    window.electron.on('deviceFound', listen)
+    window.electron.on('foundDevicesUpdate', listen)
+    return () => {
+      window.electron.removeListener('deviceFound', listen)
+      window.electron.removeListener('foundDevicesUpdate', listen)
+    }
+  }, [])
+  if (!deviceData) {
+    // ** can render something else until a device is discovered
+    return <></>
+  }
+  const devices = deviceData.devices.map((device) => {
     return (
-        <div className="mt-6 grid grid-cols-1 justify-between  divide-y-2 ">
-            {devices}
-        </div>
+      <Device
+        key={device.address}
+        userName={device.username}
+        platform={device.platform}
+        address={device.address}
+      />
     )
+  })
+  return (
+    <div className="mt-6 grid grid-cols-1 justify-between  divide-y-2 ">
+      {devices}
+    </div>
+  )
 }
 
 export default DiscoveredDevices
